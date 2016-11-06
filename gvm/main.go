b54dbeb7e2e4f9520f408fa9df861316083ed538
tantexian <tantexian@qq.com>
2016-11-06 21:47:03 +0800

    增加idea的main方式调试注释

--- gvm/main.go
+++ gvm/main.go
@@ -17,6 +17,11 @@ import (
    	在Go语言中，main是一个特殊的包，这个包所在的目录（可以叫作任何名字）会被编译为可执行文件。
    	Go程序的入口也是main（）函数，但是不接收任何参数，也不能有返回值。
 
+   	注意：1/若希望使用idea的run/debug功能，则选择+ Go Application
+   	     2/然后Package设置为wishgo4jvm/gvm即可
+   	     3/启动参数放置到Program arguments项
+
+
    Author: tantexian
    Since: 2016/8/17
 */
